e8c966aaa266317b26096ae0044c44f87572566c
unex <kentonyx@gmail.com>
2023-01-10 06:16:18 +0800

    Initial Commit

---
+++ SentrienFieldServices-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SentrienFieldServices-iOS
+//
+//  Created by Kent Onyx Arintok on 1/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
